04bf234c189426cdb2495ec6c58ece0c937cb63b
tmatsuba <tma@M1-MacBook-Pro.local>
2022-04-06 09:41:17 +0900

    Selection SortをPythonで実装

--- algorithm/python/test/test_sort.py
+++ algorithm/python/test/test_sort.py
@@ -1,8 +1,14 @@
 from algorithm.bubblesort import bubble_sort
+from algorithm.selectionsort import selection_sort
 
 def test_bubble_sort():
     assert bubble_sort([]) == []
     assert bubble_sort([1]) == [1]
     assert bubble_sort([1, 3, 2, 4, 7, 6, 8, 5]) == [1, 2, 3, 4, 5, 6, 7, 8]
+
+def test_selection_sort():
+    assert selection_sort([]) == []
+    assert selection_sort([1]) == [1]
+    assert selection_sort([1, 3, 2, 4, 7, 6, 8, 5]) == [1, 2, 3, 4, 5, 6, 7, 8]
 
 
